82171c973fd98d286fbe05097db75121bec5474c
Studio Kaori <studiokaori.com@gmail.com>
2022-05-31 21:46:23 +0200

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Kaori Persson on 2022-05-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
