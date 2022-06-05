7de294bb7e2f532836d8647ad83e4cfcfd9708d7
Studio Kaori <studiokaori.com@gmail.com>
2022-06-05 21:16:28 +0200

    Add: conversations table

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class ConversationsViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        // hide the conversation since it's initially empty
+        table.isHidden = true
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+       let label = UILabel()
+        label.text = "No conversations"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.addSubview(tableView)
+        view.addSubview(noConversationsLabel)
+        setupTableView()
+        fetchConversations()
     }
 
     // check if the user is logged in or not
@@ -30,6 +54,28 @@ class ConversationsViewController: UIViewController {
             present(nav, animated: false)
         }
     }
+    
+    private func fetchConversations() {
+        
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 
+}
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
 }
 
