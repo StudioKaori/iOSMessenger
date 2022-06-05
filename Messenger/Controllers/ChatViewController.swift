721120f3ce4b775f833d574a2dd81c8bcd556a70
Studio Kaori <studiokaori.com@gmail.com>
2022-06-05 22:07:47 +0200

    Add: dummy chat

--- Messenger/Controllers/ChatViewController.swift
+++ Messenger/Controllers/ChatViewController.swift
@@ -22,12 +22,47 @@ struct Sender: SenderType {
 }
 
 class ChatViewController: MessagesViewController {
+    
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Joe Smith")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender,
+                               messageId: "1",
+                               sentDate: Date(),
+                               kind: .text("Hello World message")))
+        messages.append(Message(sender: selfSender,
+                               messageId: "1",
+                               sentDate: Date(),
+                               kind: .text("Hello World message, Hello World message")))
 
         view.backgroundColor = .red
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+
+}
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    
+    func currentSender() -> SenderType {
+        return selfSender
     }
     
-
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    
 }
