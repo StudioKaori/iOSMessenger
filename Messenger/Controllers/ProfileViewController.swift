aa05500a0c0d7249608f396752e8a037dafbc3a8
Studio Kaori <studiokaori.com@gmail.com>
2022-06-05 14:06:59 +0200

    Add: FBlog out

--- Messenger/Controllers/ProfileViewController.swift
+++ Messenger/Controllers/ProfileViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseAuth
+import FBSDKLoginKit
 
 class ProfileViewController: UIViewController {
 
@@ -53,6 +54,9 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
                 guard let strongSelf = self else {
                     return
                 }
+                
+                // Log out FB
+                FBSDKLoginKit.LoginManager().logOut()
                 
                 do {
                     try FirebaseAuth.Auth.auth().signOut()
