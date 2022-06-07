9e3900ea41d0c9bd1c2526273518026f9d76332a
Studio Kaori <studiokaori.com@gmail.com>
2022-06-07 12:39:50 +0200

    Add: profile image to the profile VC

--- Messenger/Controllers/ProfileViewController.swift
+++ Messenger/Controllers/ProfileViewController.swift
@@ -51,9 +51,37 @@ class ProfileViewController: UIViewController {
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.borderWidth = 3
         imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageView.width/2
         headerView.addSubview(imageView)
         
+        StorageManager.shared.downloadURL(for: path, completion: { [weak self] result in
+            switch result {
+            case .success(let url):
+                self?.downloadImage(imageView: imageView, url: url)
+            case .failure(let error):
+                print("Failed to get download url: \(path), Error: \(error)")
+            }
+        })
+        
         return headerView
+    }
+    
+    func downloadImage(imageView: UIImageView, url: URL) {
+        URLSession.shared.dataTask(with: url,
+                                   completionHandler: { data, _, error in
+            guard let data = data, error == nil else {
+                print("Failed to download the image from the path: \(url)")
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                imageView.image = image
+            }
+        }).resume()
     }
 
 }
