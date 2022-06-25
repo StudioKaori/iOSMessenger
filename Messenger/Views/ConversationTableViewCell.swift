6f9c8cab7def820784c32ae75ab708ec08fdca97
Studio Kaori <studiokaori.com@gmail.com>
2022-06-25 08:09:36 +0200

    Add: 3 subviews to tableview cell

--- Messenger/Views/ConversationTableViewCell.swift
+++ Messenger/Views/ConversationTableViewCell.swift
@@ -8,6 +8,29 @@
 import UIKit
 
 class ConversationTableViewCell: UITableViewCell {
+    
+    // Generate cell's 3 subviews
+    private let userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 50
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private let userNameLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 21, weight: .semibold)
+        return label
+    }()
+    
+    private let userMessageLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 19, weight: .regular)
+        label.numberOfLines = 0
+        return label
+    }()
+    
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -19,6 +42,9 @@ class ConversationTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        contentView.addSubview(userImageView)
+        contentView.addSubview(userNameLabel)
+        contentView.addSubview(userMessageLabel)
     }
     
     public func configure(with mode: String) {
