1b7d7837f453144b011e13a960c2897e9265dd3f
Studio Kaori <studiokaori.com@gmail.com>
2022-06-26 07:42:00 +0200

    Update: add success completion

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -372,6 +372,8 @@ extension DatabaseManager {
                                     latestMessage: latestMessageObject)
                 
             })
+            
+            completion(.success(conversations))
         })
     }
     
